4b60cd64bcfed23916c74b0d328cad8879213f0a
Terence Onyekachi <87383877+Terencino8@users.noreply.github.com>
2023-04-12 07:58:32 +0100

    finished the second quote sction but the whole site has a little over flow

--- src/components/quoteSeection2/Quotesection2.js
+++ src/components/quoteSeection2/Quotesection2.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import "./Quotesection2.css"
+import logo from "../../assets/logo.png"
+
 
 const Quotesection2 = () => {
   return (
-    <div>
-        <h1>Quotesection2</h1>
-    </div>
+    <section id='quotesection2'>
+        <div className="container quotesection2">
+            <img src={logo} alt="logo" />
+
+
+
+            <h1>Precious Child Palace Academy</h1>
+            <p className="u-text-small u-text-light ">
+            with international standards as we take stride towards meeting up with the world's 21st century demand in educating every child, providing quality and standardised education that takes care of all areas of child's life such as moral, emotional, mental, physical, social, spiritual and so on. We on ground with our well trained, competent and dedicated educators in languages eg French, jolly phonics, diction etiquette and manners, Montessori and other extra moral lessons coupled with other learning activities gearing towards bringing out the very best in any child who school here. <br></br>We nurture ! We love! We care! We are PCPA!
+            </p>
+        </div>
+    </section>
   )
 }
 
